cd6e4f59e0fa6362a79c5436b316a4984bdd9b7a
Nifer Kilakila <nifercritter@gmail.com>
2022-09-12 23:18:18 -0700

    Experimenting with components, aside half working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Aside from "./components/Aside";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Testimonials from "./components/Testimonials";
+import About from "./components/About/About";
+// import Contact from "./components/Contact";
+import Aside from "./components/Aside/Aside";
+// import Projects from "./components/Projects";
+// import Skills from "./components/Skills";
+// import Testimonials from "./components/Testimonials";
 
 export default function App() {
   return (
     <main>
       <Aside />
       <About />
-      <Projects />
+      {/* <Projects />
       <Skills />
       <Testimonials />
-      <Contact />
+      <Contact /> */}
     </main>
   );
 }
--- src/components/Aside/Aside.js
+++ src/components/Aside/Aside.js
@@ -1,10 +1,49 @@
 import { useState } from 'react';
+import SidebarMenu from 'react-bootstrap-sidebar-menu';
 
-export const Aside = () => {
+
+
+export default function Aside() {
   return (
-    <div>
-     
-    </div>
+<SidebarMenu>
+  <SidebarMenu.Header>
+    <SidebarMenu.Brand>
+      {/* Your brand icon */}
+    </SidebarMenu.Brand>
+    <SidebarMenu.Toggle />
+  </SidebarMenu.Header>
+  <SidebarMenu.Body>
+    <SidebarMenu.Nav>
+      <SidebarMenu.Nav.Link>
+        <SidebarMenu.Nav.Icon>
+          {/* Menu item icon */}
+        </SidebarMenu.Nav.Icon>
+        <SidebarMenu.Nav.Title>
+          <p>About Me</p>
+        </SidebarMenu.Nav.Title>
+      </SidebarMenu.Nav.Link>
+    </SidebarMenu.Nav>
+    <SidebarMenu.Sub>
+      <SidebarMenu.Sub.Toggle>
+        <SidebarMenu.Nav.Icon />
+        <SidebarMenu.Nav.Title>
+          {/* Submenu title */}
+        </SidebarMenu.Nav.Title>
+      </SidebarMenu.Sub.Toggle>
+      <SidebarMenu.Sub.Collapse>
+        <SidebarMenu.Nav>
+          <SidebarMenu.Nav.Link>
+            <SidebarMenu.Nav.Icon>
+              {/* Submenu item icon */}
+            </SidebarMenu.Nav.Icon>
+            <SidebarMenu.Nav.Title>
+              {/* Submenu item title */}
+            </SidebarMenu.Nav.Title>
+          </SidebarMenu.Nav.Link>
+        </SidebarMenu.Nav>
+      </SidebarMenu.Sub.Collapse>
+    </SidebarMenu.Sub>
+  </SidebarMenu.Body>
+</SidebarMenu>
   )
 }
-
