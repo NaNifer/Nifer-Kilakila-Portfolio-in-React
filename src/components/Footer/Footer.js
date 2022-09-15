27dba85fe4b0f370284bfb29d47ca4d00abb2c11
Nifer Kilakila <nifercritter@gmail.com>
2022-09-14 22:30:59 -0700

    Working through Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,28 +2,45 @@
 // import "./Footer.css";
 
 
+
 const Footer = () => {
     return (
-        <footer class="bg-light text-center text-white">
-            <div class="container p-4 pb-0">
-                <section class="mb-4">
+        <footer className="bg-danger text-center text-white">
+            <div className="container p-4 pb-0">
+                <section className="mb-4">
 
 
-                    <dd>
-                        <a href="mailto:niferkilakila@gmail.com"><i class="icon fa fa-envelope"></i></a>
+                    {/* <dd>
+                        <a href="mailto:niferkilakila@gmail.com"><i className="icon fa fa-envelope"></i></a>
                     </dd>
                     <dd>
-                        <a href="https://www.linkedin.com/in/nifer-kilakila-b0721565/"><i class="icon fa fa-linkedin"></i></a>
+                        <a href="https://www.linkedin.com/in/nifer-kilakila-b0721565/"><i className="icon fa fa-linkedin"></i></a>
                     </dd>
                     <dd>
-                        <a href="https://github.com/NaNifer"><i class="icon fa fa-github"></i></a>
+                        <a href="https://github.com/NaNifer"><i className="icon fa fa-github"></i></a>
                     </dd>
                     <dd>
-                        <a href="https://stackoverflow.com/users/18479722/nifer"><i class="icon fa fa-twitter"></i></a>
-                    </dd>
+                        <a href="https://stackoverflow.com/users/18479722/nifer"><i className="icon fa fa-twitter"></i></a>
+                    </dd> */}
+                    {/* Linkedin */}
+                    <a
+                        className="btn text-white btn-floating m-1"
+                        // style="background-color: #0082ca;"
+                        href="#!"
+                        role="button"
+                    ><i className="fab fa-linkedin-in"></i
+                    ></a>
+                    {/* Github */}
+                    <a
+                        className="btn text-white btn-floating m-1"
+                        // style="background-color: #333333;"
+                        href="#!"
+                        role="button"
+                    ><i className="fab fa-github"></i
+                    ></a>
 
                 </section>
-                <div class="text-center p-3">
+                <div className="text-center p-3">
                     2022 Nifer Kilakila
                 </div>
             </div>
