79cdd7beb2fb21aca182b8dd0f9420eb88afac78
Nifer Kilakila <nifercritter@gmail.com>
2022-10-05 22:33:23 -0700

    defaults and supports es6-module

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,6 @@
 //import useState hook to create menu collapse state
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 //import react pro sidebar components
 import {
