d0e56df8833a3cead6276345af7773eb41d728ab
Nifer Kilakila <nifercritter@gmail.com>
2023-02-13 13:21:18 -0800

    debugging form

--- server/server.js
+++ server/server.js
@@ -1,47 +1,53 @@
-const express = require("express");
-const router = express.Router();
-const cors = require("cors");
-const nodemailer = require("nodemailer");
-
+const path = require('path');
+const express = require('express');
+const transporter = require('./config');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
-app.use(cors());
+
+const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.json());
-app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+app.use(express.static(buildPath));
 
-const contactEmail = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: "niferkilakila@gmail.com",
-      pass: process.env.gmailPW,
-    },
-  });
-  
-  contactEmail.verify((error) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Ready to Send");
-    }
-  });
+app.post('/send', (req, res) => {
+  try {
+    const mailOptions = {
+      from: req.body.email, // sender address
+      to: process.env.email, // list of receivers
+      subject: req.body.subject, // Subject line
+      html: `
+      <p>You have a new contact request.</p>
+      <h3>Contact Details</h3>
+      <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+        <li>Subject: ${req.body.subject}</li>
+        <li>Message: ${req.body.message}</li>
+      </ul>
+      `
+    };
 
-  // router.post("/contact", (req, res) => {
-  //   const name = req.body.name;
-  //   const email = req.body.email;
-  //   const message = req.body.message; 
-  //   const mail = {
-  //     from: name,
-  //     to: "***************@gmail.com",
-  //     subject: "Contact Form Submission",
-  //     html: `<p>Name: ${name}</p>
-  //            <p>Email: ${email}</p>
-  //            <p>Message: ${message}</p>`,
-  //   };
-  //   contactEmail.sendMail(mail, (error) => {
-  //     if (error) {
-  //       res.json({ status: "ERROR" });
-  //     } else {
-  //       res.json({ status: "Message Sent" });
-  //     }
-  //   });
-  // });
+    transporter.sendMail(mailOptions, function (err, info) {
+      if (err) {
+        res.status(500).send({
+          success: false,
+          message: 'Something went wrong. Try again later'
+        });
+      } else {
+        res.send({
+          success: true,
+          message: 'Thanks for reaching out. I will get back to you shortly'
+        });
+      }
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: 'Something went wrong. Try again later'
+    });
+  }
+});
+
+app.listen(3030, () => {
+  console.log('server start on port 3030');
+});
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -11,8 +11,9 @@ const ContactForm = () => {
     subject: '',
     message: ''
   });
-  
+
   const [result, setResult] = useState(null);
+  
   const sendEmail = event => {
     event.preventDefault();
     axios
@@ -36,6 +37,11 @@ const ContactForm = () => {
   };
   return (
     <div>
+      {result && (
+        <p className={`${result.success ? 'success' : 'error'}`}>
+          {result.message}
+        </p>
+      )}
       <form onSubmit={sendEmail}>
         <Form.Group controlId="name">
           <Form.Label>Full Name: </Form.Label>
@@ -85,47 +91,5 @@ const ContactForm = () => {
     </div>
   );
 };
-
-
-// const ContactForm = () => {
-//   const [status, setStatus] = useState("Submit");
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     setStatus("Sending...");
-//     const { name, email, message } = e.target.elements;
-//     let details = {
-//       name: name.value,
-//       email: email.value,
-//       message: message.value,
-//     };
-//     let response = await fetch("http://localhost:5000/contact", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json;charset=utf-8",
-//       },
-//       body: JSON.stringify(details),
-//     });
-//     setStatus("Submit");
-//     let result = await response.json();
-//     alert(result.status);
-//   };
-//   return (
-//     <Form onSubmit={handleSubmit}>
-//       <Form.Group className="mb-3">
-//         <Form.Label htmlFor="name">Name:</Form.Label>
-//         <Form.Control type="text" id="name" placeholder="Ada Lovelace" required />
-//       </Form.Group >
-//       <Form.Group className="mb-3">
-//         <Form.Label htmlFor="email">Email:</Form.Label>
-//         <Form.Control type="email" id="email" placeholder="ada@rocks.com" required />
-//       </Form.Group >
-//       <Form.Group className="mb-3">
-//         <Form.Label htmlFor="message">Message:</Form.Label>
-//         <Form.Control id="message" placeholder="Looking forward to hearing from you!" required />
-//       </Form.Group >
-//       <Button className='btn btn-info text-white' type="submit">{status}</Button>
-//     </Form>
-//   );
-// };
 
 export default ContactForm;
