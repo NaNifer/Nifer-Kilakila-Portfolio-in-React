7a15ed351ed4d35e30d5266e011636c94bb564a7
Nifer Kilakila <nifercritter@gmail.com>
2023-02-14 13:02:32 -0800

    redeploy

--- server/config.js
+++ server/config.js
@@ -2,7 +2,6 @@ const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
